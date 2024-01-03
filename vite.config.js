9dd4939eb9b7a670cbdd677604739e0d6466fa84
AvivGoldgamer <aviv240@gmail.com>
2024-01-03 02:17:01 +0200

    Changes assetsfilename

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,12 @@ import svgr from "vite-plugin-svgr";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svgr(), react()],
-  base: "",
+  build: {
+    assetsDir: "src/assets",
+    rollupOptions: {
+      output: {
+        assetFileNames: "assets/[name][extname]",
+      },
+    },
+  },
 });
